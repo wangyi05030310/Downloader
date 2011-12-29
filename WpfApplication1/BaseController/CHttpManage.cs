4a1bfb4c44845e7f724383a95e9ada4f2a7db03b
wangyi05030310 <wangyi05030310@gmail.com>
2011-12-29 23:09:06 +0800

    WYb realize a demo of Http transmission with delegate and event.

--- WpfApplication1/BaseController/CHttpManage.cs
+++ WpfApplication1/BaseController/CHttpManage.cs
@@ -1,11 +1,150 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
+using System.Windows;
+using System.Threading;
+using System.IO;
+
+using WpfApplication1.BaseController.Facilitation;
 
 namespace WpfApplication1.BaseController
 {
     class CHttpManage
     {
+        public bool[] threadEnd;
+        public string[] snippet;
+        public int[] fileStart;
+        public int[] fileSize;
+        public string theUrl;
+        private string FileName;
+        private string location;
+        private bool combination;
+        public int threadTotality;
+
+        private Thread[] threadRunning;
+        private CHttpSnippet[] model;
+
+        public CHttpManage(string url, string loc, int num)
+        {
+            theUrl = url;
+            location = loc;
+            threadTotality = num;
+
+            if (location.LastIndexOf("\\") != location.Length - 1)
+            {
+                location += "\\";
+            }
+
+            FileName = theUrl.Substring(theUrl.LastIndexOf('/'));
+        }
+
+        public void startDownload()
+        {
+            DateTime dt = DateTime.Now;
+            //TODO: send an message to UI of start time
+
+            HttpWebRequest request;
+            long filesize = 0;
+
+            try
+            {
+                request = (HttpWebRequest)HttpWebRequest.Create(theUrl);
+                filesize = request.GetResponse().ContentLength;
+                request.Abort();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Error in stardDownload:" + ex.Message);
+            }
+
+            threadEnd = new bool[threadTotality];
+            snippet = new string[threadTotality];
+            fileStart = new int[threadTotality];
+            fileSize = new int[threadTotality];
+
+            int filethread = (int)filesize / threadTotality;
+            int filethreadrest = filethread + (int)filesize % threadTotality;
+
+            for (int i = 0; i < threadTotality;++i )
+            {
+                threadEnd[i] = false;
+                snippet[i] = i.ToString() + ".temp";
+                if (i < threadTotality - 1)
+                {
+                    fileStart[i] = filethread * i;
+                    fileSize[i] = filethread - 1;
+                }
+                else
+                {
+                    fileStart[i] = filethread * i;
+                    fileSize[i] = filethread - 1;
+                }
+            }
+
+            threadRunning = new Thread[threadTotality];
+            model = new CHttpSnippet[threadTotality];
+
+            for (int i = 0; i < threadTotality;++i )
+            {
+                model[i] = new CHttpSnippet(this, i);
+                model[i].EndEvent += new HttpEventHandler(OnEndReceive);
+                threadRunning[i] = new Thread(new ThreadStart(model[i].receive));
+                threadRunning[i].Start();
+            }
+
+            Thread combineThread = new Thread(new ThreadStart(combineFile));
+            combineThread.Start();
+        }
+
+        private void combineFile()
+        {
+            while (true)
+            {
+                combination = true;
+                for (int i = 0; i < threadTotality;++i )
+                {
+                    if (threadEnd[i] == false)
+                    {
+                        combination = false;
+                        Thread.Sleep(100);
+                        break;
+                    }
+                }
+                if (combination == true)
+                {
+                    break;
+                }
+            }
+
+            FileStream fs;
+            FileStream fsTemp;
+            int readfile;
+            byte[] bytes = new byte[512];
+            fs = new FileStream(location + FileName,FileMode.Create);
+            for (int i = 0; i < threadTotality;++i )
+            {
+                fsTemp = new FileStream(snippet[i], FileMode.Open);
+                while (true)
+                {
+                    readfile = fsTemp.Read(bytes, 0, 512);
+                    if (readfile > 0)
+                    {
+                        fs.Write(bytes, 0, readfile);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                fsTemp.Close();
+            }
+            fs.Close();
+            DateTime dt = DateTime.Now;
+            //TODO: send an event to UI of the end time
+        }
+
+        private void OnEndReceive(object sender, EventArgs e)
+        {
+            //TODO: Reply the snippet of the download finished event
+        }
     }
 }
---
+++ WpfApplication1/BaseController/Facilitation/CHttpSnippet.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Windows;
+
+using WpfApplication1.BaseController;
+
+namespace WpfApplication1.BaseController.Facilitation
+{
+    public delegate void HttpEventHandler(object sender, EventArgs e);
+
+    class CHttpSnippet
+    {
+        private CHttpManage threadManage;
+        private int threadNumber;
+        private string fileName;
+        private string theurl;
+        private FileStream fs;
+        private HttpWebRequest request;
+        private Stream iostream;
+        private byte[] nbytes;
+        private int nreadsize;
+
+        public event HttpEventHandler EndEvent;
+
+        public CHttpSnippet(CHttpManage controller, int thread)
+        {
+            threadManage = controller;
+            threadNumber = thread;
+        }
+
+        public void initiate()
+        {
+            fileName = threadManage.snippet[threadNumber];
+            theurl = threadManage.theUrl;
+            iostream = null;
+            nbytes = new byte[512];
+            nreadsize = -1;
+            fs = new FileStream(fileName, FileMode.Create);
+
+            //TODO: send an event to UI where a snippet start downloading
+        }
+
+        public void receive()
+        {
+            initiate();
+
+            try
+            {
+                request = (HttpWebRequest)HttpWebRequest.Create(theurl);
+
+                request.AddRange(threadManage.fileStart[threadNumber],
+                    threadManage.fileStart[threadNumber] + threadManage.fileSize[threadNumber]);
+                iostream = request.GetResponse().GetResponseStream();
+
+                nreadsize = iostream.Read(nbytes, 0, 512);
+                while (nreadsize > 0)
+                {
+                    fs.Write(nbytes, 0, nreadsize);
+                    nreadsize = iostream.Read(nbytes, 0, 512);
+                    //TODO: send an event to UI where something'll change
+
+                }
+                fs.Close();
+                iostream.Close();
+
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Error in snippet receiving:" + ex.Message);
+                fs.Close();
+            }
+
+
+            EndReceive();
+            threadManage.threadEnd[threadNumber] = true;
+        }
+
+        private void EndReceive()
+        {
+            //TODO: send a event to UI where a snippet have reach an end
+            EndEvent(this, null);
+        }
+    }
+}
