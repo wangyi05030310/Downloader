800199123fed09ea453968f2e94d4d1d7a5d45ec
Andriy_Lin <lxk19921205@gmail.com>
2012-01-02 22:57:59 +0800

    在CFtpServerInfo中增加了从URL读name和pwd

--- WpfApplication1/ElementEntity/CFtpServerInfo.cs
+++ WpfApplication1/ElementEntity/CFtpServerInfo.cs
@@ -6,11 +6,14 @@ using System.Text;
 namespace WpfApplication1.ElementEntity
 {
     /// <summary>
-    /// 登陸的ftp服務器的信息
+    /// 登录的ftp服務器相关的信息
     /// </summary>
     class CFtpServerInfo
     {
+        public readonly string FTP_PREFIX = "ftp://";
+
         private string server_addr;
+        private string file_path;
         private string user_name;
         private string user_pwd;
 
@@ -21,6 +24,15 @@ namespace WpfApplication1.ElementEntity
         {
             set { server_addr = value; }
             get { return server_addr; }
+        }
+
+        /// <summary>
+        /// 此文件在FTP server下的路径，包括最开始的'/'
+        /// </summary>
+        public string FilePath
+        {
+            set { file_path = value; }
+            get { return file_path; }
         }
 
         /// <summary>
@@ -41,11 +53,62 @@ namespace WpfApplication1.ElementEntity
             get { return user_pwd; }
         }
 
-        public CFtpServerInfo(string server_addr, string user_name = "", string user_pwd = "")
+        public CFtpServerInfo(string url)
         {
-            this.server_addr = server_addr;
-            this.user_name = user_name;
-            this.user_pwd = user_pwd;
+            url = url.Trim();
+            if (!url.StartsWith("ftp://", StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new System.ArgumentException("url should start with \"ftp://\"");
+            }
+
+            url = url.Substring(6);
+            int index = url.IndexOf('@');
+            if (index != -1)
+            {
+                //@ found
+                string name_pwd = url.Substring(0, index);
+                this.decodeNamePwd(name_pwd, out user_name, out user_pwd);
+                url = url.Substring(index + 1);
+            }
+
+            index = url.IndexOf('/');
+            if (index != -1)
+            {
+                server_addr = url.Substring(0, index);
+                file_path = url.Substring(index);
+            }
+            else
+            {
+                server_addr = url;
+                file_path = "";
+            }
+        }
+
+        /// <summary>
+        /// 根据FTP的下载URL的规则，从中读出用户名和密码
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="name"></param>
+        /// <param name="pwd"></param>
+        private void decodeNamePwd(string src, out string name, out string pwd)
+        {
+            int index = src.IndexOf(':');
+            if (index == -1)
+            {
+                throw new ArgumentException("下载路径非法！应该含有':'作为用户名和密码的分隔符");
+            }
+
+            name = src.Substring(0, index);
+            pwd = src.Substring(index + 1);
+        }
+
+        /// <summary>
+        /// 返回完整的下载URL
+        /// </summary>
+        /// <returns></returns>
+        public string getFullUrl()
+        {
+            return server_addr + file_path;
         }
     }
 }
