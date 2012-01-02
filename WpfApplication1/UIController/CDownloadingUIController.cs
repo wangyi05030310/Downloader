36b7ec9c668fc2a5cc5e9a3f0266b3dfdc7eabe5
Andriy_Lin <lxk19921205@gmail.com>
2012-01-02 22:08:57 +0800

    在UIController包里边增加了三个类，下载中、已下载、FTP的界面控制

---
+++ WpfApplication1/UIController/CDownloadingUIController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1.UIController
+{
+    /// <summary>
+    /// 正在下载部分的界面控制
+    /// </summary>
+    class CDownloadingUIController
+    {
+    }
+}
---
+++ WpfApplication1/UIController/CFTPUIController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1.UIController
+{
+    /// <summary>
+    /// FTP资源部分的界面控制
+    /// </summary>
+    class CFTPUIController
+    {
+    }
+}
