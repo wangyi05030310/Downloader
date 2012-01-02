7ccbec05ce6a375692974e8c314962e80b5fb624
Andriy_Lin <lxk19921205@gmail.com>
2012-01-02 22:03:38 +0800

    在CFtpServerInfo中增加了一些信息，虽然不知道会不会用到

--- WpfApplication1/ElementEntity/CFtpServerInfo.cs
+++ WpfApplication1/ElementEntity/CFtpServerInfo.cs
@@ -10,5 +10,42 @@ namespace WpfApplication1.ElementEntity
     /// </summary>
     class CFtpServerInfo
     {
+        private string server_addr;
+        private string user_name;
+        private string user_pwd;
+
+        /// <summary>
+        /// FTP server的地址，IP或者非IP
+        /// </summary>
+        public string ServerAddr
+        {
+            set { server_addr = value; }
+            get { return server_addr; }
+        }
+
+        /// <summary>
+        /// 登录FTP server的用户名
+        /// </summary>
+        public string UserName
+        {
+            set { user_name = value; }
+            get { return user_name; }
+        }
+
+        /// <summary>
+        /// 登录FTP server的密码
+        /// </summary>
+        public string UserPwd
+        {
+            set { user_pwd = value; }
+            get { return user_pwd; }
+        }
+
+        public CFtpServerInfo(string server_addr, string user_name = "", string user_pwd = "")
+        {
+            this.server_addr = server_addr;
+            this.user_name = user_name;
+            this.user_pwd = user_pwd;
+        }
     }
 }
