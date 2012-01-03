c835768b3e85cdd38db44167e8ac532ae4fa1103
Andriy Lin <lxk19921205@gmail.com>
2012-01-03 09:51:14 +0800

    增加了FileExistedException

---
+++ WpfApplication1/Exceptions/FileExistedException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1.Exceptions
+{
+    /// <summary>
+    /// 下载要创建文件的时候，目标文件已经存在了
+    /// </summary>
+    class FileExistedException : Exception
+    {
+        public FileExistedException(string info)
+            : base(info)
+        {
+        }
+    }
+}
