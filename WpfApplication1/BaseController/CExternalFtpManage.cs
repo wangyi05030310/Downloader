a90ee6eafb2fd7f72e20f9003b2ecfef3e5d6282
Andriy_Lin <lxk19921205@gmail.com>
2012-01-04 10:33:51 +0800

    同上，估计要把所有的ftp操作合在一个类里了

--- WpfApplication1/BaseController/CExternalFtpManage.cs
+++ WpfApplication1/BaseController/CExternalFtpManage.cs
@@ -28,6 +28,50 @@ namespace WpfApplication1.BaseController
         }
     }
 
+    class DownloadState
+    {
+        private string m_target_path;
+        private long m_total_size;
+        private long m_current_size;
+        private Thread m_thread;
+
+        /// <summary>
+        /// 下载到哪里
+        /// </summary>
+        public string TargetPath
+        {
+            set { m_target_path = value; }
+            get { return m_target_path; }
+        }
+
+        /// <summary>
+        /// 文件总大小
+        /// </summary>
+        public long TotalSize
+        {
+            set { m_total_size = value; }
+            get { return m_total_size; }
+        }
+
+        /// <summary>
+        /// 当前已下载了多少
+        /// </summary>
+        public long CurrentSize
+        {
+            set { m_current_size = value; }
+            get { return m_current_size; }
+        }
+
+        /// <summary>
+        /// 用于下载的线程
+        /// </summary>
+        public Thread DownloadThread
+        {
+            set { m_thread = value; }
+            get { return m_thread; }
+        }
+    }
+
     /// <summary>
     /// 外部FTP资源的下载管理，操作下载
     /// 注：此FTP下载没有多线程下载，没有断点续传
@@ -49,20 +93,13 @@ namespace WpfApplication1.BaseController
         {
         }
 
-        private string m_target_path;
-
-        private long m_total_size;
-        private long m_current_size;
-
-        private FileStream m_fs;
-        private Thread m_downloadThread;
-
+        private DownloadState m_download_state = new DownloadState();
         /// <summary>
         /// 文件总大小
         /// </summary>
         public long FileSize
         {
-            get { return m_total_size; }
+            get { return m_download_state.TotalSize; }
         }
 
         /// <summary>
@@ -78,7 +115,7 @@ namespace WpfApplication1.BaseController
         /// </summary>
         public long CurrentSize
         {
-            get { return m_current_size; }
+            get { return m_download_state.CurrentSize; }
         }
 
         public delegate void FtpDownloadHandler(object sender, EventArgs args);
@@ -116,10 +153,10 @@ namespace WpfApplication1.BaseController
                 throw new FileExistedException("此路径已存在，又没有说要不要覆盖");
             }
 
-            m_target_path = targetPath + '\\' + m_ftpInfo.FileName;
+            m_download_state.TargetPath = targetPath + '\\' + m_ftpInfo.FileName;
 
-            m_downloadThread = new Thread(new ThreadStart(doDownload));
-            m_downloadThread.Start();
+            m_download_state.DownloadThread = new Thread(new ThreadStart(doDownload));
+            m_download_state.DownloadThread.Start();
             onDownloadStarted(this, null);
         }
 
@@ -130,8 +167,8 @@ namespace WpfApplication1.BaseController
         {
             this.retrieveFileSize();
 
-            m_fs = File.Create(m_target_path);
-            m_fs.SetLength(m_total_size);
+            FileStream fs = File.Create(m_download_state.TargetPath);
+            fs.SetLength(m_download_state.TotalSize);
 
             FtpWebRequest request = (FtpWebRequest)WebRequest.Create(m_ftpInfo.getFullUrl());
             request.EnableSsl = m_enable_ssh;
@@ -151,21 +188,20 @@ namespace WpfApplication1.BaseController
             {
                 throw ex;
             }
-            m_current_size = 0;
+            m_download_state.CurrentSize = 0;
             byte[] buffer = new byte[BUFF_SIZE];
             int readCount;
             do
             {
                 readCount = remoteStream.Read(buffer, 0, BUFF_SIZE);
-                m_fs.Write(buffer, 0, readCount);
-                m_current_size += readCount;
+                fs.Write(buffer, 0, readCount);
+                m_download_state.CurrentSize += readCount;
                 onDownloadAdvanced(this, null);
             } while (readCount > 0);
 
             remoteStream.Close();
             response.Close();
-            m_fs.Close();
-            m_fs = null;
+            fs.Close();
 
             onDownloadFinished(this, null);
         }
@@ -194,11 +230,208 @@ namespace WpfApplication1.BaseController
             {
                 throw ex;
             }
-            m_total_size = response.ContentLength;
+            m_download_state.TotalSize = response.ContentLength;
             response.Close();
 
             onFileSizeRetrieved(this, null);
         }
 
+
+        /// <summary>
+        /// 上传文件到FTP上
+        /// </summary>
+        /// <param name="fileurl"></param>
+        public void upload(string fileurl, string fileName)
+        {
+            FileInfo fileInf = null;
+            try
+            {
+                fileInf = new FileInfo(fileurl);
+            }
+            catch (System.Exception)
+            {
+                throw new FilePathInvalidException("传进来的文件路径有问题！");
+            }
+
+            FtpWebRequest request = (FtpWebRequest)FtpWebRequest.Create(m_ftpInfo.getFullUrl() + fileName);
+            request.Credentials = new NetworkCredential(m_ftpInfo.UserName, m_ftpInfo.UserPwd);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.UseBinary = true;
+            request.ContentLength = fileInf.Length;
+            request.EnableSsl = m_enable_ssh;
+
+            byte[] buff = new byte[BUFF_SIZE];
+            int readCount;
+
+            FileStream fs = fileInf.OpenRead();
+            Stream stream = request.GetRequestStream();
+            do
+            {
+                readCount = fs.Read(buff, 0, BUFF_SIZE);
+                stream.Write(buff, 0, readCount);
+            } while (readCount > 0);
+            stream.Close();
+            fs.Close();
+
+            try
+            {
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            }
+            catch (System.Exception ex)
+            {
+                //这个时候才报异常，可能是网络啥的问题
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 拿到ftp上此目录下所有文件名
+        /// </summary>
+        /// <returns></returns>
+        public string[] getFileList()
+        {
+            List<string> files = new List<string>();
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(m_ftpInfo.getFullUrl());
+            request.UseBinary = true;
+            request.EnableSsl = m_enable_ssh;
+            request.Credentials = new NetworkCredential(m_ftpInfo.UserName, m_ftpInfo.UserPwd);
+            request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+            FtpWebResponse response = null;
+            StreamReader reader = null;
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+                reader = new StreamReader(response.GetResponseStream());
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    files.Add(line);
+                    line = reader.ReadLine();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+            return files.ToArray();
+        }
+
+        /// <summary>
+        /// 删除指定的文件
+        /// </summary>
+        public void delete(string fileName)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(m_ftpInfo.getFullUrl() + fileName);
+            request.Credentials = new NetworkCredential(m_ftpInfo.UserName, m_ftpInfo.UserPwd);
+            request.EnableSsl = m_enable_ssh;
+            request.Method = WebRequestMethods.Ftp.DeleteFile;
+            request.UseBinary = true;
+
+            try
+            {
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 在FTP上创建一个目录
+        /// </summary>
+        /// <param name="dir"></param>
+        public void makeDir(string dir)
+        {
+
+        }
+
+        /// <summary>
+        /// 在FTP上删除一个目录
+        /// </summary>
+        public void deleteDir()
+        {
+
+        }
+
+        /// <summary>
+        /// 改名！
+        /// </summary>
+        public void renameFile(string fileName, string newName)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(m_ftpInfo.getFullUrl() + fileName);
+            request.Credentials = new NetworkCredential(m_ftpInfo.UserName, m_ftpInfo.UserPwd);
+            request.EnableSsl = m_enable_ssh;
+            request.Method = WebRequestMethods.Ftp.Rename;
+            request.UseBinary = true;
+            request.RenameTo = newName;
+
+            try
+            {
+                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 获得一个含有详细信息的string[]
+        /// </summary>
+        /// <returns></returns>
+        public string[] getFileDetailList()
+        {
+            List<string> details = new List<string>();
+
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(m_ftpInfo.getFullUrl());
+            request.UseBinary = true;
+            request.EnableSsl = m_enable_ssh;
+            request.Credentials = new NetworkCredential(m_ftpInfo.UserName, m_ftpInfo.UserPwd);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            FtpWebResponse response = null;
+            StreamReader reader = null;
+            try
+            {
+                response = (FtpWebResponse)request.GetResponse();
+                reader = new StreamReader(response.GetResponseStream());
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    details.Add(line);
+                    line = reader.ReadLine();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+            return details.ToArray();
+        }
     }
 }
